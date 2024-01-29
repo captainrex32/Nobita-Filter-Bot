69b05a437949abb968f08583eb3eb5b96ff3a575
Chayan <150181325+captainrex32@users.noreply.github.com>
2024-01-29 21:22:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
-bif [ -z $UPSTREAM_REPO ]
-then
+bif [ -z $UPSTREAM_REPO ] then
   echo "Cloning main Repository"
   git clone https://github.com/NobiDeveloper/Nobita-Filter-Bot.git /Nobita-Filter-Bot
 else
